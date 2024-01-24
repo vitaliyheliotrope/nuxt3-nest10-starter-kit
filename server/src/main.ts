17b0ea9266631ffefa438b19ae286b6c127ad171
vitaliyheliotrope <vitaliy@heliotrope.digital>
2024-01-24 12:11:32 +0200

    Add start script to package.json and update server main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { exec } from 'child_process';
+import { exec, execSync } from 'child_process';
 
-const isDev = false;
+const isDev = true;
 
 async function bootstrap() {
-  eval(`import('nuxt')`).then(async ({ loadNuxt, build }) => {
-    const app = await NestFactory.create(AppModule);
-    if (isDev) {
-      exec('npm run dev', { cwd: '..' });
-    } else {
-      exec('npm run build', { cwd: '..' });
-      exec('npm run start', { cwd: '..' });
-    }
-
-    await app.listen(3001);
-  });
+  if (isDev) {
+    exec('npm run dev', { cwd: '..' });
+  } else {
+    execSync('rm -rf .output', { cwd: '..' });
+    execSync('npm run build', { cwd: '..' });
+    execSync('npm run start', { cwd: '..' });
+  }
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
 }
+
 bootstrap();
